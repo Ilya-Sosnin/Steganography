d36f5ec691f28cd459687a6933312a08bd243069
Ilya_Sosnin <ilja.sosnin2014@yandex.ru>
2026-04-12 23:39:30 +0700

    Added parsing of command parameters

--- src/app/app/main.cpp
+++ src/app/app/main.cpp
@@ -1,7 +1,62 @@
 #include "CLI/CLI.hpp"
-#include <charconv>
-#include <string_view>
 
 #include "lab1/ImageProcessor.hpp"
 
-int main(int argc, char *argv[]) {}
+int main(int argc, char *argv[]) {
+
+  CLI::App app{};
+
+  int bitNumber = 0;
+  string mode;
+  fs::path image;
+  fs::path message;
+
+  app.add_option("--mode", mode)->required();
+  app.add_option("--bit", bitNumber)->required();
+  app.add_option("--image", image)->required();
+  auto msgOpt = app.add_option("--message", message);
+
+  app.parse(argc, argv);
+
+  if (bitNumber < 1 || bitNumber > 8) {
+    std::cout << "--bit must be between 1 and 8\n";
+    return -1;
+  }
+
+  fs::path imagePath = fs::path(PROJECT_ROOT) / image;
+  if (!fs::exists(imagePath)) {
+    std::cout << "--image does not exist\n";
+    return -1;
+  }
+
+  if (mode == "embed") {
+    if (!msgOpt->count()) {
+      std::cout << "---message required for embed\n";
+      return -1;
+    }
+  }
+
+  ImageProcessor processor;
+
+  if (mode == "plane") {
+    processor.ExtBitPlane(bitNumber, imagePath);
+  } else if (mode == "embed") {
+    if (!msgOpt->count()) {
+      std::cout << "--message required for embed\n";
+      return -1;
+    }
+    fs::path messagePath = fs::path(PROJECT_ROOT) / message;
+    if (!fs::exists(messagePath)) {
+      std::cout << "--message does not exist\n";
+      return -1;
+    }
+    processor.EmbeddingData(bitNumber, imagePath, messagePath);
+  } else if (mode == "extract") {
+    processor.ExtMessage(bitNumber, imagePath);
+  } else {
+    cout << "Error: unknown mode\n";
+    return -1;
+  }
+
+  return 0;
+}
